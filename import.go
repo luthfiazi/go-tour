efdb317fafbe1e87b4670fcbce021273f7ef16e0
luthfiazi <luthfiazizi11@gmail.com>
2021-10-05 16:32:18 +0700

    try go tour

---
+++ import.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main(){
+	fmt.Printf("Sekarang saya memiliki %g masalah.\n", math.Sqrt(10))
+}
---
+++ sandbox.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main(){
+	fmt.Println("Selamat datang Master")
+	fmt.Println("Waktu terus berjalan", time.Now())
+}
